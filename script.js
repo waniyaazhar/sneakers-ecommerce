375d2e9fab68113e5e0db1a7b4b98316133f74e8
Waniya Azhar <waniyaazhar24@gmail.com>
2026-05-05 21:23:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeColor() {
+    const card = document.querySelector('.product-card');
+    card.style.borderColor = '#2f3542';
+    card.style.borderWidth = '2px';
+    card.style.borderStyle = 'solid';
+    console.log("Maintenance mode: Style adjusted via JS.");
+}
